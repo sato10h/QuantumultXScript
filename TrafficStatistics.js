5c89d0e5e802928b642fe8a2f1bf28ca71a3bf3c
sato10h <128966876+sato10h@users.noreply.github.com>
2023-03-26 18:37:51 +0900

    Add files via upload

---
+++ TrafficStatistics.js
@@ -0,0 +1,18 @@
+// Auto Check In
+// 该脚本用于自动签到
+// https://github.com/ConnersHua/Profiles/blob/master/Quantumult/Task/AutoCheckin.conf
+
+const wifi = $network.wifi.ssid;
+
+if (wifi === "MyHomeWifi") {
+  const checkinUrl = "https://example.com/checkin";
+  $httpClient.get(checkinUrl, (error, response, data) => {
+    if (error) {
+      console.log(error);
+      $notification.post("Auto Check In", "Failed", error);
+    } else {
+      console.log(data);
+      $notification.post("Auto Check In", "Success", data);
+    }
+  });
+}
